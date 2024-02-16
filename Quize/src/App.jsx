fb8e6b1dbd95cc4c2ceaa07c5e669169dd620ae1
MUKILAN019 <mukilan.p@kalvium.community>
2024-02-16 17:45:18 +0530

    added files

---
+++ Quize/src/App.jsx
@@ -0,0 +1,20 @@
+
+import Home from "./Components/HomeComponent"
+import Quiz from "./Components/QuizeComponent"
+import Result from "./Components/ResultComponent"
+
+function App(){
+  return(
+    <div>
+{/* <Home/>
+<Quiz/>
+<Result/> */}
+<Home/>
+<Quiz/>
+<Result/>
+
+    </div>
+  )
+}
+
+export default App
